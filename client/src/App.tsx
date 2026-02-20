ca57c8cad245b7103c36aaa47918cdb9ba21a32f
Jaskaran2907 <jaskaransingh290706@gmail.com>
2026-02-20 23:58:57 +0530

    different dropdowns added in the UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import React, { useEffect , useState } from "react";
 import Title from "./component/title";
 import FrontPageNews from "./component/frontPageNews";
@@ -18,6 +19,9 @@ function App() {
   const [keyword , setKeyword] = useState("");
   const [frontPageData , setFrontPageData] = useState<NewsArticle[]>([]);
   const [specificData , setSpecificData] = useState<NewsArticle[]>([]);
+  const [Category , setCategory] = useState<string>("");
+  const [country , setCountry] = useState<string>("");
+  const [language , setLanguage] = useState<string>("");
 
   const dataToRender = specificData.length>0 ? specificData : frontPageData;
 
@@ -40,7 +44,7 @@ function App() {
         "Content-Type":"application/json"
       },
 
-      body:JSON.stringify({message:keyword})
+      body:JSON.stringify({message:keyword } )
     });
     
     const specific_data = await result.json();
@@ -65,18 +69,107 @@ function App() {
       <Title />
 
       <div className="container my-4">
-        <div className="row ">
-          <div className="col-lg-6 offset-lg-3 col-9 offset-1 ">
-            <div className="row m-0 p-0 d-flex justify-content-center">
+        <div className="row align-items-stretch">
 
-              <div className="col-md-8 col-9">
-                <input placeholder="Enter a Keyword" className="form-control" onChange={handleChange} onKeyDown={handleKeydown}></input>
-              </div>
+          <div className="col-md-auto col-4 dropdown d-flex justify-content-end align-items-end">
+            <button className="btn btn-secondary dropdown-toggle btn-dropdown " type="button" data-bs-toggle="dropdown">Country</button>
+            <ul className="dropdown-menu">
+              <li className="dropdown-item" onClick={() => setCountry("ae")}>UAE</li>
+              <li className="dropdown-item" onClick={() => setCountry("ar")}>Argentina</li>
+              <li className="dropdown-item" onClick={() => setCountry("at")}>Austria</li>
+              <li className="dropdown-item" onClick={() => setCountry("au")}>Australia</li>
+              <li className="dropdown-item" onClick={() => setCountry("be")}>Belgium</li>
+              <li className="dropdown-item" onClick={() => setCountry("bg")}>Bulgaria</li>
+              <li className="dropdown-item" onClick={() => setCountry("br")}>Brazil</li>
+              <li className="dropdown-item" onClick={() => setCountry("ca")}>Canada</li>
+              <li className="dropdown-item" onClick={() => setCountry("ch")}>Switzerland</li>
+              <li className="dropdown-item" onClick={() => setCountry("cn")}>China</li>
+              <li className="dropdown-item" onClick={() => setCountry("co")}>Colombia</li>
+              <li className="dropdown-item" onClick={() => setCountry("cu")}>Cuba</li>
+              <li className="dropdown-item" onClick={() => setCountry("cz")}>Czech Republic</li>
+              <li className="dropdown-item" onClick={() => setCountry("de")}>Germany</li>
+              <li className="dropdown-item" onClick={() => setCountry("eg")}>Egypt</li>
+              <li className="dropdown-item" onClick={() => setCountry("fr")}>France</li>
+              <li className="dropdown-item" onClick={() => setCountry("gb")}>United Kingdom</li>
+              <li className="dropdown-item" onClick={() => setCountry("gr")}>Greece</li>
+              <li className="dropdown-item" onClick={() => setCountry("hk")}>Hong Kong</li>
+              <li className="dropdown-item" onClick={() => setCountry("hu")}>Hungary</li>
+              <li className="dropdown-item" onClick={() => setCountry("id")}>Indonesia</li>
+              <li className="dropdown-item" onClick={() => setCountry("ie")}>Ireland</li>
+              <li className="dropdown-item" onClick={() => setCountry("il")}>Israel</li>
+              <li className="dropdown-item" onClick={() => setCountry("in")}>India</li>
+              <li className="dropdown-item" onClick={() => setCountry("it")}>Italy</li>
+              <li className="dropdown-item" onClick={() => setCountry("jp")}>Japan</li>
+              <li className="dropdown-item" onClick={() => setCountry("kr")}>South Korea</li>
+              <li className="dropdown-item" onClick={() => setCountry("lt")}>Lithuania</li>
+              <li className="dropdown-item" onClick={() => setCountry("lv")}>Latvia</li>
+              <li className="dropdown-item" onClick={() => setCountry("ma")}>Morocco</li>
+              <li className="dropdown-item" onClick={() => setCountry("mx")}>Mexico</li>
+              <li className="dropdown-item" onClick={() => setCountry("my")}>Malaysia</li>
+              <li className="dropdown-item" onClick={() => setCountry("ng")}>Nigeria</li>
+              <li className="dropdown-item" onClick={() => setCountry("nl")}>Netherlands</li>
+              <li className="dropdown-item" onClick={() => setCountry("no")}>Norway</li>
+              <li className="dropdown-item" onClick={() => setCountry("nz")}>New Zealand</li>
+              <li className="dropdown-item" onClick={() => setCountry("ph")}>Philippines</li>
+              <li className="dropdown-item" onClick={() => setCountry("pl")}>Poland</li>
+              <li className="dropdown-item" onClick={() => setCountry("pt")}>Portugal</li>
+              <li className="dropdown-item" onClick={() => setCountry("ro")}>Romania</li>
+              <li className="dropdown-item" onClick={() => setCountry("rs")}>Serbia</li>
+              <li className="dropdown-item" onClick={() => setCountry("ru")}>Russia</li>
+              <li className="dropdown-item" onClick={() => setCountry("sa")}>Saudi Arabia</li>
+              <li className="dropdown-item" onClick={() => setCountry("se")}>Sweden</li>
+              <li className="dropdown-item" onClick={() => setCountry("sg")}>Singapore</li>
+              <li className="dropdown-item" onClick={() => setCountry("si")}>Slovenia</li>
+              <li className="dropdown-item" onClick={() => setCountry("sk")}>Slovakia</li>
+              <li className="dropdown-item" onClick={() => setCountry("th")}>Thailand</li>
+              <li className="dropdown-item" onClick={() => setCountry("tr")}>Turkey</li>
+              <li className="dropdown-item" onClick={() => setCountry("tw")}>Taiwan</li>
+              <li className="dropdown-item" onClick={() => setCountry("ua")}>Ukraine</li>
+              <li className="dropdown-item" onClick={() => setCountry("us")}>United States</li>
+              <li className="dropdown-item" onClick={() => setCountry("ve")}>Venezuela</li>
+              <li className="dropdown-item" onClick={() => setCountry("za")}>South Africa</li>
+            </ul>
+          </div>
 
-              <div className="col-md-4 col-3">
-                <button className="btn btn-primary for-btn" onClick={handleSubmit} >Search</button>
-              </div>
-            </div> 
+          <div className="col-md-auto col-4 dropdown d-flex justify-content-center align-items-end">
+            <button className="btn btn-secondary dropdown-toggle btn-dropdown" type="button" data-bs-toggle="dropdown">Language</button>
+            <ul className="dropdown-menu">
+              <li className="dropdown-item" onClick={() => setLanguage("ar")}>Arabic</li>
+              <li className="dropdown-item" onClick={() => setLanguage("de")}>German</li>
+              <li className="dropdown-item" onClick={() => setLanguage("en")}>English</li>
+              <li className="dropdown-item" onClick={() => setLanguage("es")}>Spanish</li>
+              <li className="dropdown-item" onClick={() => setLanguage("fr")}>French</li>
+              <li className="dropdown-item" onClick={() => setLanguage("he")}>Hebrew</li>
+              <li className="dropdown-item" onClick={() => setLanguage("it")}>Italian</li>
+              <li className="dropdown-item" onClick={() => setLanguage("nl")}>Dutch</li>
+              <li className="dropdown-item" onClick={() => setLanguage("no")}>Norwegian</li>
+              <li className="dropdown-item" onClick={() => setLanguage("pt")}>Portuguese</li>
+              <li className="dropdown-item" onClick={() => setLanguage("ru")}>Russian</li>
+              <li className="dropdown-item" onClick={() => setLanguage("sv")}>Swedish</li>
+              <li className="dropdown-item" onClick={() => setLanguage("ud")}>Urdu</li>
+              <li className="dropdown-item" onClick={() => setLanguage("zh")}>Chinese</li>
+            </ul>
+          </div>
+
+          <div className="col-md-auto col-4 dropdown d-flex justify-content-start align-items-end">
+            <button className="btn btn-secondary dropdown-toggle btn-dropdown" type="button" data-bs-toggle="dropdown">Category</button>
+            <ul className="dropdown-menu">
+              <li className="dropdown-item " onClick={ () => setCategory("business")}>Business</li>
+              <li className="dropdown-item " onClick={ () => setCategory("technology")}>Technology</li>
+              <li className="dropdown-item " onClick={ () => setCategory("entertainment")}>Entertainment</li>
+              <li className="dropdown-item " onClick={ () => setCategory("general")}>General</li>
+              <li className="dropdown-item " onClick={ () => setCategory("health")}>Health</li>
+              <li className="dropdown-item " onClick={ () => setCategory("science")}>Science</li>
+              <li className="dropdown-item " onClick={ () => setCategory("sports")}>Sports</li>
+            </ul>
+          </div>
+
+          <div className="col-md col-8 mt-4">
+            <input placeholder="Enter a Keyword" className="form-control" onChange={handleChange} onKeyDown={handleKeydown}></input>
+          </div>
+
+          <div className="col-md-2 col-4 mt-4">
+            <button className="btn btn-primary for-btn h-100 w-100" onClick={handleSubmit} >Search</button>
           </div>
         </div>
       </div>
--- server/index.js
+++ server/index.js
@@ -17,24 +17,33 @@ app.use(express.json());
 
 app.post("/news/fetch", async(req , res)=>{
     const keyword = req.body.message;
-    const result = await fetch(`https://newsapi.org/v2/everything?q=${keyword}&apiKey=${process.env.api_key}`);
+    const category = req.body.category;
+
+    const result = await fetch(`https://newsapi.org/v2/top-headlines?q=${keyword}&apiKey=${process.env.api_key}`);
     const specific_news_fetch = await result.json();
+    console.log(specific_news_fetch)
     
     const required_specific_data = [];
 
     for(let i=0 ; i<specific_news_fetch.articles.length ; i++){
 
-        const cleaned_specific_data = {
-            author:specific_news_fetch.articles[i].author,
-            description:specific_news_fetch.articles[i].description,
-            publishedAt:specific_news_fetch.articles[i].publishedAt,
-            title:specific_news_fetch.articles[i].title,
-            //source:specific_news_fetch.articles[i].source.name,
-            image:specific_news_fetch.articles[i].urlToImage,
-            url:specific_news_fetch.articles[i].url
-        }
+        if(specific_news_fetch.articles[i].urlToImage){
 
-        required_specific_data.push(cleaned_specific_data);
+            const cleaned_specific_data = {
+                author:specific_news_fetch.articles[i].author,
+                description:specific_news_fetch.articles[i].description,
+                publishedAt:specific_news_fetch.articles[i].publishedAt,
+                title:specific_news_fetch.articles[i].title,
+                //source:specific_news_fetch.articles[i].source.name,
+                image:specific_news_fetch.articles[i].urlToImage,
+                url:specific_news_fetch.articles[i].url
+            }
+    
+            required_specific_data.push(cleaned_specific_data);
+
+        }else{
+            continue
+        }
 
     }
 
@@ -43,23 +52,29 @@ app.post("/news/fetch", async(req , res)=>{
 });
 
 app.get("/front_page/headlines" , async(req , res)=>{
-    const frontPageFetch = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.api_key}`);
+    const frontPageFetch = await fetch(`https://newsapi.org/v2/top-headlines?category=business&apiKey=${process.env.api_key}`);
     const front_page_fetch_converted  = await frontPageFetch.json();
+    console.log(front_page_fetch_converted);
     const required_front_page_data = [];
 
     for(let i=0 ; i<front_page_fetch_converted.articles.length ; i++){
 
-        const cleaned_front_page_data = {
-            author:front_page_fetch_converted.articles[i].author,
-            description:front_page_fetch_converted.articles[i].description,
-            publishedAt:front_page_fetch_converted.articles[i].publishedAt,
-            title:front_page_fetch_converted.articles[i].title,
-            //source:front_page_fetch_converted.articles[i].source.name,
-            image:front_page_fetch_converted.articles[i].urlToImage,
-            url:front_page_fetch_converted.articles[i].url
+        if(front_page_fetch_converted.articles[i].urlToImage){
+            
+            const cleaned_front_page_data = {
+                author:front_page_fetch_converted.articles[i].author,
+                description:front_page_fetch_converted.articles[i].description,
+                publishedAt:front_page_fetch_converted.articles[i].publishedAt,
+                title:front_page_fetch_converted.articles[i].title,
+                //source:front_page_fetch_converted.articles[i].source.name,
+                image:front_page_fetch_converted.articles[i].urlToImage,
+                url:front_page_fetch_converted.articles[i].url
+            }
+    
+            required_front_page_data.push(cleaned_front_page_data);
+        }else{
+            continue;
         }
-
-        required_front_page_data.push(cleaned_front_page_data);
 
     }
 
